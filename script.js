fcdcdb65c2f209e650e4488a41a1bca3b48bc622
NexaScaleCloud <tushar.m365@outlook.com>
2026-04-14 12:20:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-script.js
+const faders = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+    faders.forEach(el => {
+        const rect = el.getBoundingClientRect().top;
+        if (rect < window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
