e907f40b7e1f1643a907d87a7c5f8d7b37ce0dc6
NexaScaleCloud <tushar.m365@outlook.com>
2026-04-14 14:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,23 +8,23 @@ let particles = [];
 
 for (let i = 0; i < 40; i++) {
   particles.push({
-    x: Math.random()*canvas.width,
-    y: Math.random()*canvas.height,
-    vx: Math.random()-0.5,
-    vy: Math.random()-0.5
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    vx: Math.random() - 0.5,
+    vy: Math.random() - 0.5
   });
 }
 
-function animate(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  particles.forEach(p=>{
-    p.x+=p.vx;
-    p.y+=p.vy;
+  particles.forEach(p => {
+    p.x += p.vx;
+    p.y += p.vy;
 
     ctx.beginPath();
-    ctx.arc(p.x,p.y,2,0,Math.PI*2);
-    ctx.fillStyle="#38bdf8";
+    ctx.arc(p.x, p.y, 2, 0, Math.PI * 2);
+    ctx.fillStyle = "#38bdf8";
     ctx.fill();
   });
 
